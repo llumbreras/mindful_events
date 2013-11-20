eed033a6d30b8c421ff6a51509932f506f492177
L Lumbreras <llumbreras@gmail.com>
2013-11-19 22:14:50 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MindfulEvents::Application.config.secret_token = 'e36103cb609eb78c12470235bafa46e447e11f3d8ad50af602b63fd3a8e1184aa2e44bae18e2012f33f7008cbbd01170b27f8b82734c7bd197bd0f3d1f1ecd82'
