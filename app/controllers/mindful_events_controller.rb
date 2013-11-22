a1e1d2c161672849d2f3734bdcab81b5e9159d2a
L Lumbreras <llumbreras@gmail.com>
2013-11-21 20:05:48 -0800

    Change data displayed from static to dynamic

--- app/controllers/mindful_events_controller.rb
+++ app/controllers/mindful_events_controller.rb
@@ -1,6 +1,22 @@
+Mindful_event = Struct.new :status, :location
+
 class MindfulEventsController < ApplicationController
 
   def scheduled_today
+    @mindful_events = Object.new
+    def @mindful_events.not_started(&block)
+      [
+        Mindful_event.new("not_started", "San Francisco"),
+        Mindful_event.new("not_started", "San Jose"),
+        Mindful_event.new("not_started", "Santa Maria")
+      ].each(&block)
+    end
+    def @mindful_events.in_session(&block)
+      [
+        Mindful_event.new("in_session", "Santa Ana"),
+        Mindful_event.new("in_session", "Santa Cruz")
+      ].each(&block)
+    end
   end
 
 end
